bf2823d854dc180eaf8f2fe1c9f62454f41b90eb
kyle-mckay <83886427+kyle-mckay@users.noreply.github.com>
2022-09-19 21:00:09 -0600

    Update ffmpeg.py

--- ffmpeg.py
+++ ffmpeg.py
@@ -4,6 +4,8 @@ import glob
 import shutil
 import sqlite3
 from tabnanny import verbose
+from re import search
+
 sScriptPath = os.path.dirname(os.path.realpath(__file__)) + "/" 
 sConfigPath = sScriptPath + "config.py"
 sDatabasePath = sScriptPath + "contents.db"
@@ -30,7 +32,7 @@ def db_create():
     connection = sqlite3.connect('contents.db')
     cursor = connection.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS Disk_Content
-                (Current_Bits INT, Pixel_Height INT, Target_Bits INT, Target_Height INT, Encode INT, Root_path TEXT, File_Path UNIQUE)''')
+                (Current_Bits INT, Pixel_Height TEXT, Target_Bits INT, Target_Height TEXT, Encode INT, Root_path TEXT, File_Path UNIQUE)''')
     
     connection.commit()
     connection.close()
@@ -66,16 +68,12 @@ def open_file(filename):
     except AttributeError:
         subprocess.call(['open', filename])
 
-def db_scan_new():
-    # Scans directories for files and add's them to the database based on config settings
-    print("This still needs work")
-
 def db_remove_old():
     # Queries database for items that were scanned in a path no longer part of the "Root Path" and removes them
-    print("This still needs work")
+    vprint([2,"This still needs work"])
 
 def db_wipe():
-    print("This still need work")
+    print([2,"This still need work"])
 
 def create_config():
     open(sConfigPath, 'x')
@@ -102,6 +100,78 @@ bRemoveBeforeScan = True # If `True` then  all files in `sEncodePath` are delete
 bEncodeAfterScan = True # If `False` then once the CSV is created the script skips the encoding process entirely. If `True` then the script will encode all identified files after the CSV is generated.\n\
 iThreads = 2 # The number of cpu threads you wish to dedicate to ffmpeg. ")
 
+def connecttodb():
+    global cursor, sqlite_insert_with_param, sqliteConnection
+    sqliteConnection = sqlite3.connect('contents.db')
+    cursor = sqliteConnection.cursor()
+    vprint([1,"Connected to SQLite"])
+
+    sqlite_insert_with_param = """INSERT INTO Disk_Content
+                        (Current_Bits, Pixel_Height, Target_Bits, Target_Height, Encode, Root_Path, File_Path) 
+                        VALUES (?, ?, ?, ?, ?, ?, ?);"""
+def disconnectdb():
+    cursor.close()
+    if sqliteConnection:
+            sqliteConnection.close()
+            vprint([1,"The SQLite connection is closed"])
+
+def insertVaribleIntoTable(Current_Bits, Pixel_Height, Target_Bits, Target_Height, Encode, Root_Path, File_Path):
+    try:
+
+        data_tuple = (Current_Bits, Pixel_Height, Target_Bits, Target_Height, Encode, Root_Path, File_Path)
+        cursor.execute(sqlite_insert_with_param, data_tuple)
+        sqliteConnection.commit()
+        vprint([1,f"File inserted successfully into Disk_Content table, {File_Path}"])
+
+    except sqlite3.Error as error:
+        string = "UNIQUE constraint failed" 
+        if search(string, str(error)):
+            vprint([1,f"File already in DB, skipping - {File_Path}"])
+        else:
+            vprint([3,f"Failed to insert Python variable into sqlite table, {error}"])
+
+def check_ignore(path):
+    vprint([2,'check_ignore requires building'])
+    return False
+
+def ffprobe_bits():
+    vprint([2,'ffprobe_bits requires building'])
+    return 100
+
+def ffprobe_height():
+    vprint([2,'ffprobe_height requires building'])
+    return 1080
+
+def calc_target_bits():
+    vprint([2,'calc_target_bits requires building'])
+    return 70
+
+def calc_target_height():
+    vprint([2,'calc_target_height requires building'])
+    return 720
+
+def calc_encode():
+    vprint([2,'calc_encode requires building'])
+    return True
+
+def db_scan_new():
+    # traverse root directory, and list directories as dirs and files as files
+    connecttodb()
+    vprint([2,'Need to add logic to change depending on if recursive search is enabled'])
+    for root, _, files in os.walk(config.sRootPath):
+        for file in files:
+            # file returns the file name and extension
+            fullpath = os.path.join(root,file) # join root folders for full path
+            if check_ignore(fullpath):
+                vprint([1,f'.ignore file detected for {fullpath}'])
+            else:
+                Current_Bits = ffprobe_bits()
+                Pixel_Height = ffprobe_height()
+                Target_Bits = calc_target_bits()
+                Target_Height = calc_target_height()
+                if calc_encode():
+                    insertVaribleIntoTable(Current_Bits, Pixel_Height, Target_Bits, Target_Height, 1, config.sRootPath, fullpath)  
+    disconnectdb()
 
 def fscan():
     global sRootPath, sEncodePath
